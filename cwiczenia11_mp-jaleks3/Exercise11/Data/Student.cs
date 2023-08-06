3d8386efe706b20d291e10ffa087a73d60e06c3b
jaleks3 <127289882+jaleks3@users.noreply.github.com>
2023-08-07 00:36:36 +0200

    Add files via upload

---
+++ cwiczenia11_mp-jaleks3/Exercise11/Data/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise11.Data
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Avatar { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Studies { get; set; }
+    }
+}
